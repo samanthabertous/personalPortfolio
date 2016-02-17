1cabf9392e765113b76e739e22dd7b6201b7ed19
Samantha D. Smith <sam@secretlyshameless.com>
2016-02-16 22:13:16 -0500

    fixed moblie issue of extra space on right side

--- index.php
+++ index.php
@@ -16,55 +16,35 @@
 		<section class = "work_content" id = "work">
 		<div class= "skew">	
 			<a name="work">
-			<div id ="my_experience" id ="about">
-				<div class= "skew">	
-				<h3 id="experience">MY EXPERIENCE</h3>
-			</div>
-			</div>
-		
+				<div class= "JS_work">
+					<ul>
+						<li>
+							<a href="projects/Slideshow/index.html">Slideshow with Thumbnails</a>
+						</li>
+						<li><a href="/projects/to_do_list/indexCompleted.html" >To Do List</a></li>
+						<li><a href="projects/rock_paper_scissors/index_rockPaperScissors.html" >Rock. Paper. Scissors.</a></li>
+						<li><a href="projects/tic_tac_toe/index.html">TIC TAC TOE</a></li>
+						<li><a href="projects/velocity_animation/grid_velocity.html">Hundreds of Squares Animation with Velocity</a></li>
+						<li><a href="#">API Weather Page</a></li>
+						<li><a href="projects/tabs/index.html" >Recipe Tabs</a></li>
+					</ul> 
+				</div>
+		</div>
 
-			</div>
 			
-			<!-- <div class= "JS_work">
-				<ul>
-					<li><a href="projects/Slideshow/index.html" target="_blank">Slideshow with Thumbnails</a></li>
-					<li><a href="projects/to_do_list/indexCompleted.html" target="_blank">To Do List</a></li>
-					<li><a href="projects/rock_paper_scissors/index_rockPaperScissors.html" target="_blank">Rock. Paper. Scissors.</a></li>
-					<li><a href="projects/tic_tac_toe/index.html " target="_blank">TIC TAC TOE</a></li>
-					<li><a href="projects/velocity_animation/grid_velocity.html">Hundreds of Squares Animation with Velocity</a></li>
-					<li><a href="#">API Weather Page</a></li>
-					<li><a href="projects/tabs/index.html" target="_blank">Recipe Tabs</a></li>
-				</ul> 
-			</div>-->
-
 			<button id= "github_button"><a href="https://github.com/secretlyShameless"target="_blank">LOOK AT MY CODE</a></button>
 
-		</div>
-		</section>
-
-<?php
-	include("inc/resume_content.php");
-?>
-
-
-
-
-		<section class = "blog_content" id = "blog">
-		<div class= "skew">	
-			<a name="blog">
 		</section>
 
 
-		<section class = "mainfooter">
+		<section id = "mainfooter">
 			<a name="contact">
-			<div class = "skew">
 				<footer>
 					<a href="#Contact"></a>
 					<p> &copy; 2015 Secretly Shameless Design Studio<br/>
 					sam@secretlyshameless.com</p>
 
 				</footer>
-			</div>
 		<section>
 
 
