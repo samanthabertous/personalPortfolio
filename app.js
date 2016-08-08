f897eeda232ffa29fc1dabb5e25ba2013d863f36
Samantha Bretous <sam@samanthabretous.com>
2016-08-07 23:19:00 -0400

    added work projects with express and internal json files

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
 var express = require("express");
 var app = express();
+var fs = require('fs');
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+var web_projects_results;
+var graphic_projects_results
+fs.readFile('json/web_projects.json', 'utf8', function(err, data){
+  if(err){
+    throw err;
+  } else {
+    web_projects_results = JSON.parse(data)
+  }
+})
+fs.readFile('json/graphic_projects.json', 'utf8', function(err, data){
+  if(err){
+    throw err;
+  } else {
+    graphic_projects_results = JSON.parse(data)
+  }
+})
+
 
 // ========================
 //ROUTES
 // ========================
 
 app.get("/", function(req, res) {
-  res.render("index")
+  res.render("index", {
+    title: 'Home',
+    web_projects: web_projects_results,
+    graphic_projects : graphic_projects_results
+  });
 });
 
 app.get("/about", function(req, res) {
 
 })
 
-app.get("/work", function(req, res) {
-
+app.get("/projects", function(req, res) {
+    res.render("projects", {
+    title: 'Projects',
+    projects: results
+  });
 })
 
 app.get("*", function(req, res) {
